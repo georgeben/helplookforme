8b8e294927e72811bc9114d61d44e1c598f610a2
George Benjamin <msdcconnect@gmail.com>
2019-12-11 13:44:52 +0100

    add fotter to app component

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,18 @@
     </div> -->
     <Navbar />
     <router-view/>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Shared/Navbar.vue';
+import Footer from '@/components/Shared/Footer.vue';
 export default {
   name: 'app',
   components: {
     Navbar,
+    Footer
   }
 }
 </script>
