99b4fad712652951b8fe89b61bd13a38ddfdda8d
George Kurobara Benjamin <msdcconnect@gmail.com>
2019-12-12 18:41:23 +0100

    Create case page (#3)
    * create report case form1
    
    * create report case form2
    
    * create report case form3
    
    * scroll to top of page when switching between forms
    
    * create report case form4

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Auth from '../views/Auth.vue'
+import ReportCase from '../views/ReportCase'
 import Login from '@/components/Auth/Login.vue'
 import Signup from '@/components/Auth/SignUp.vue'
 
@@ -11,7 +12,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
   },
   {
     path: '/about',
@@ -19,7 +20,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
     path: '/auth',
@@ -27,19 +29,23 @@ const routes = [
     children: [
       {
         path: '',
-        redirect: 'login'
+        redirect: 'login',
       },
       {
         path: 'login',
-        component: Login
+        component: Login,
       },
       {
         path: 'signup',
-        component: Signup
-      }
-    ]
-  }
-]
+        component: Signup,
+      },
+    ],
+  },
+  {
+    path: '/report-case',
+    component: ReportCase,
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
