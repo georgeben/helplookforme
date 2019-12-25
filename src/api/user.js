634874bdf1d31401cf5abae5b52ecac4c95788f6
George Benjamin <msdcconnect@gmail.com>
2019-12-25 18:51:46 +0100

    display cases reported by users

--- src/api/user.js
+++ src/api/user.js
@@ -2,13 +2,18 @@ import { httpClient } from './httpClient';
 const endpoint = '/users';
 
 async function getUserData() {
-  return httpClient.get(`${endpoint}`);
+  return httpClient.get(endpoint);
+}
+
+async function getUserCases() {
+  return httpClient.get(`${endpoint}/cases`);
 }
 
 async function updateUserProfile(payload) {
-  return httpClient.put(`${endpoint}`, payload);
+  return httpClient.put(endpoint, payload);
 }
 export default {
   getUserData,
   updateUserProfile,
-}
+  getUserCases,
+};
