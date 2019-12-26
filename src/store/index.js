fd44b904392d160d9ca1de0249baa3281d29f871
George Kurobara Benjamin <msdcconnect@gmail.com>
2019-12-26 19:15:23 +0100

    Ft browse cases (#11)
    * fetch cases and display on browser cases page
    
    * view single case

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,11 @@ const debug = process.env.NODE_ENV !== 'production';
 const vuexLocal = new VuexPersistence({
   key: 'hlfm',
   storage: window.localStorage,
-})
+  reducer: state => ({
+    Auth: state.Auth,
+    User: state.User,
+  }),
+});
 export default new Vuex.Store({
   strict: debug,
   modules,
