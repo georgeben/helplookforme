6773a1d1435303fe69c3339a95cf619f11737c96
George Benjamin <msdcconnect@gmail.com>
2020-01-13 11:14:34 +0100

    reduce min characters for contact us message to 20 characters

--- src/schemas/contactUsSchema.js
+++ src/schemas/contactUsSchema.js
@@ -4,7 +4,7 @@ export default yup.object().shape({
   message: yup
     .string()
     .required('Please enter a message')
-    .min(70, 'Your message must be at least 70 characters long'),
+    .min(20, 'Your message must be at least 20 characters long'),
   email: yup
     .string()
     .email('Please enter a valid email')
